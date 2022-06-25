df209f4fe106a2bf1a1fce77ae41168c27a2421e
thekuwayama <thekuwayama@gmail.com>
2022-06-26 04:30:25 +0900

    implement: wasm fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,53 @@
 use base64;
-use getrandom::{getrandom, Error};
+use getrandom::getrandom;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsValue;
 
-pub fn random_bin32_base64() -> Result<String, Error> {
+#[wasm_bindgen]
+pub fn random_bin32_base64() -> Result<JsValue, JsValue> {
     let mut buf = [0u8; 32];
-    getrandom(&mut buf)?;
-    Ok(base64::encode(buf))
+    if let Err(_) = getrandom(&mut buf) {
+        return Err(JsValue::from("getrandom error"));
+    }
+
+    Ok(JsValue::from(base64::encode(buf)))
 }
 
-pub fn random_bin32_hex() -> Result<String, Error> {
+#[wasm_bindgen]
+pub fn random_bin32_hex() -> Result<JsValue, JsValue> {
     let mut buf = [0u8; 32];
-    getrandom(&mut buf)?;
-    Ok(buf.iter().map(|x| format!("{:02x}", x)).collect::<String>())
+    if let Err(_) = getrandom(&mut buf) {
+        return Err(JsValue::from("getrandom error"));
+    }
+
+    Ok(JsValue::from(
+        buf.iter().map(|x| format!("{:02x}", x)).collect::<String>(),
+    ))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
+    use wasm_bindgen_test::*;
+
+    #[wasm_bindgen_test]
     fn test_random_bin32_base64() {
-        let s = random_bin32_base64();
-        assert!(s.is_ok());
-        assert_eq!(s.unwrap().len(), 44);
+        let result = random_bin32_base64();
+        assert!(result.is_ok());
+
+        let s = result.unwrap();
+        assert!(s.is_string());
+        assert_eq!(s.as_string().unwrap().len(), 44);
     }
 
-    #[test]
+    #[wasm_bindgen_test]
     fn test_random_bin32_hex() {
-        let s = random_bin32_hex();
-        assert!(s.is_ok());
-        assert_eq!(s.unwrap().len(), 64);
+        let result = random_bin32_hex();
+        assert!(result.is_ok());
+
+        let s = result.unwrap();
+        assert!(s.is_string());
+        assert_eq!(s.as_string().unwrap().len(), 64);
     }
 }
